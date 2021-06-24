3a28116ad393deec85d2cff17a8c0cd271d6848b
vnikhori <85665387+vnikhori@users.noreply.github.com>
2021-06-24 10:29:03 +0530

    test

---
+++ src/main/java/com/example/vibrepo/VibrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.vibrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibrepoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/vibrepo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.vibrepo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	
+	@GetMapping("/")
+	public String getTest() {
+		return "test";
+	}
+
+}
