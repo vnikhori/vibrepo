4952c4fc3a8825b48e91940ecfd45ab9fd923178
vnikhori <85665387+vnikhori@users.noreply.github.com>
2021-06-24 09:51:21 +0530

    Delete ms-notification directory

--- ms-notification/src/main/java/com/ms/poc/notification/msnotification/repository/ConfirmationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ms.poc.notification.msnotification.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ms.poc.notification.msnotification.data.model.Confirmation;
-
-public interface ConfirmationRepository extends MongoRepository<Confirmation, Integer> {
-
-}
